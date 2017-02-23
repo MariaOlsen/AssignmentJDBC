6fa3df58e7823410ed581170453357b3bda33ac2
maria <maria@Warren.home>
2017-02-23 16:10:07 +0100

    Added variables "username", "password" and "conn_string" to Main class 	* Variables are used to connect to a database (postgreSQL database for testing)
    Added code to Main method: Attempts to open a connection to a postgreSQL database. The attempt is surrounded by a try-catch block, that catches an SQLException if the connection fails.
     A 'finally' block is added to the try-catch, wherein the connection is closed

--- JDBCAssignment/src/view/Main.java
+++ JDBCAssignment/src/view/Main.java
@@ -5,17 +5,37 @@
  */
 package view;
 
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author maria
  */
 public class Main {
 
+    private static final String USERNAME = "dbuser";
+    private static final String PASSWORD = "dbpassword";
+    private static final String CONN_STRING = "jdbc:postgresql://localhost:5432/explorecalifornia";
+
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws SQLException {
+
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
+        } catch (SQLException ex) {
+            System.out.println("CONNECTION FAIL");
+            System.err.println(ex);
+        } finally {
+            if (con != null) {
+                con.close();
+            }
+        }
+
     }
-    
+
 }
